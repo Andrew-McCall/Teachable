fa0f7fb4a56070d8a7ebdd52e7bce540ac48e835
Andrew-McCall <amccall@qa.com>
2022-06-20 01:00:23 +0100

    Complete $ping

--- DiscordBot/2 - $Ping/index.js
+++ DiscordBot/2 - $Ping/index.js
@@ -1,10 +1,18 @@
 const {Client, Intents} = require("discord.js");
 const {token} = require("./config.json");
 
-const client = new Client( {intents:[Intents.FLAGS.GUILDS]} );
+const client = new Client( {intents:[Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]} );
 
 client.once("ready", ()=>{
     console.log("Ready")
+})
+
+client.on("messageCreate", (message) => {
+
+    if (message.content.toLowerCase() === "$ping"){
+        message.reply(":ping_pong: Pong! :ping_pong:")
+    }
+
 })
 
 client.login(token);
